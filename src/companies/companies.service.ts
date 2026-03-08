5774ba75e11585ba7be1542a8748f99fab86b53e
loc-iparamed <loc.dp@iparamed.com>
2026-03-08 18:08:32 +0700

    CompaniesService pagination and filtering

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -5,6 +5,8 @@ import { Model, Types } from 'mongoose';
 import { Company } from './schemas/company.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import type { IUser } from 'src/users/users.interface';
+import aqp from 'api-query-params';
+import { isEmpty } from 'class-validator';
 
 @Injectable()
 export class CompaniesService {
@@ -23,8 +25,33 @@ export class CompaniesService {
     });
   }
 
-  findAll() {
-    return this.companyModel.find();
+  async findAll(currentPage: number, limit: number, qs: string) {
+    const { filter, sort, population } = aqp(qs);
+    delete filter.page;
+    delete filter.limit;
+    let offset = (+currentPage - 1) * +limit;
+    let defaultLimit = +limit ? +limit : 10;
+
+    const totalItems = (await this.companyModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.companyModel
+      .find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+      .populate(population)
+      .exec();
+
+    return {
+      meta: {
+        current: currentPage, //trang hiện tại
+        pageSize: limit, //số lượng bản ghi đã lấy
+        pages: totalPages, //tổng số trang với điều kiện query
+        total: totalItems, // tổng số phần tử (số bản ghi)
+      },
+      result, //kết quả query
+    };
   }
 
   findOne(id: number) {
